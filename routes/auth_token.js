0d2cdf8b33ec4d931a2b96e96cb76209728cb325
Luis Rojas <lrojas@tecfin212.com>
2023-08-04 17:10:55 -0400

    jwt

--- routes/auth_token.js
+++ routes/auth_token.js
@@ -1,21 +1,51 @@
 import { Router } from "express";
 import authByEmailPwd from "../helpers/authByEmailPwd.js";
-
+import { USER_BBDD } from "../src/bbdd.js";
+import { SignJWT, jwtVerify } from 'jose'
 const authTokenRouter = Router();
 
 //loggin con email
-authTokenRouter.post("/login", (req, res) => {
+authTokenRouter.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) return res.sendStatus(400);
 
   try {
-    const user = authByEmailPwd(email, password);
+    const {guid} = authByEmailPwd(email, password);
 
-    return res.send(`usuario ${user.name} autenticado`);
+    // Generar token y devolver token
+    const jwtConstructor = new SignJWT({guid})
+
+    const encoder = new TextEncoder();
+    const jwt = await jwtConstructor
+    .setProtectedHeader({alg: 'HS256', type: 'SWT'})
+    .setIssuedAt()
+    .setExpirationTime('1h').sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
+
+    return res.send({jwt});
+
   } catch (err) {
     return res.sendStatus(401);
   }
 });
+authTokenRouter.get('/profile', async (req, res) => {
+    const {authorization} = req.headers
+    if(!authorization) res.sendStatus(401)
 
+    try {
+    const encoder = new TextEncoder();
+
+        const {payload} = await jwtVerify(authorization, encoder.encode(process.env.JWT_PRIVATE_KEY));
+        const user = USER_BBDD.find(s => s.guid === payload.guid)
+        if(!user) return res.sendStatus(401)
+        delete user.password
+        return res.send(user)
+
+    }catch (err) {
+    return res.sendStatus(401);
+
+    }
+
+
+})
 export default authTokenRouter;
